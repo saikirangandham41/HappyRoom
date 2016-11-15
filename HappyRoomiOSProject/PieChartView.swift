b2ccf3fa21c2ad79474ba30c10759a356173a380
Gandham,Sai Kiran <s525905@ch140009m.nwmissouri.edu>
2016-11-14 21:17:05 -0600

    Fetched data from Parse

--- HappyRoomiOSProject/PieChartView.swift
+++ HappyRoomiOSProject/PieChartView.swift
@@ -7,15 +7,64 @@
 //
 
 import UIKit
+@IBDesignable
 
 class PieChartView: UIView {
-
-    /*
+     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
         // Drawing code
     }
     */
+    
+    
+    override func drawRect(rect: CGRect) {
+        
+                   let sliceRating5:UIBezierPath = UIBezierPath()
+                sliceRating5.moveToPoint(CGPoint(x:150.0,y:150.0))
+                sliceRating5.addArcWithCenter(CGPoint(x:150.0,y:150.0), radius: 150.0, startAngle: 0.0, endAngle: CGFloat(2*M_PI/5), clockwise: true)
+        
+                UIColor.blueColor().setFill()
+        
+                sliceRating5.fill()
+        
+                let sliceRating4:UIBezierPath = UIBezierPath()
+                sliceRating4.moveToPoint(CGPoint(x:150.0,y:150.0))
+                sliceRating4.addArcWithCenter(CGPoint(x:150.0,y:150.0), radius: 150.0, startAngle: CGFloat(2*M_PI/5), endAngle: CGFloat(4*M_PI/5), clockwise: true)
+        
+                UIColor.orangeColor().setFill()
+                
+                sliceRating4.fill()
+        let sliceRating3:UIBezierPath = UIBezierPath()
+        sliceRating3.moveToPoint(CGPoint(x:150.0,y:150.0))
+        sliceRating3.addArcWithCenter(CGPoint(x:150.0,y:150.0), radius: 150.0, startAngle: CGFloat(4*M_PI/5), endAngle: CGFloat(6*M_PI/5), clockwise: true)
+        
+        UIColor.greenColor().setFill()
+        
+        sliceRating3.fill()
+        
+        let sliceRating2:UIBezierPath = UIBezierPath()
+        sliceRating2.moveToPoint(CGPoint(x:150.0,y:150.0))
+        sliceRating2.addArcWithCenter(CGPoint(x:150.0,y:150.0), radius: 150.0, startAngle: CGFloat(6*M_PI/5), endAngle: CGFloat(8*M_PI/5), clockwise: true)
+        
+        UIColor.yellowColor().setFill()
+        
+        sliceRating2.fill()
+        let sliceRating1:UIBezierPath = UIBezierPath()
+        sliceRating1.moveToPoint(CGPoint(x:150.0,y:150.0))
+        sliceRating1.addArcWithCenter(CGPoint(x:150.0,y:150.0), radius: 150.0, startAngle: CGFloat(8*M_PI/5), endAngle: CGFloat(10*M_PI/5), clockwise: true)
+        
+        UIColor.redColor().setFill()
+        
+        sliceRating1.fill()
+
+        
+
+        
+
+       
+
+    }
 
 }
--- HappyRoomiOSProject/PiechartViewController.swift
+++ HappyRoomiOSProject/PiechartViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 import CoreData
 
 class PiechartViewController: UIViewController {
     
-    // We will be using daniel gindi framework for the graphs
+    var dataArrayWithRoomNumber:NSMutableArray = []
+    var dataArrayWithRoomStrings:NSMutableArray = []
+    var dataArrayWithRating:NSMutableArray = []
 
+    var dataDictionary:[String: [Int]] = [:]
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetch()
 
         // Do any additional setup after loading the view.
     }
@@ -34,5 +40,58 @@ class PiechartViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func fetch() {
+        let query = PFQuery(className:"Rating")
+              query.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                for object in objects!{
+                    
+                    self.dataArrayWithRoomNumber.addObject(object["roomNumber"] as! Int)
+                    self.dataArrayWithRating.addObject(object["rating"] as! Int)
+                   // dataDictionary[(dataArrayWithRoomNumber as? Int)!].append(dataArrayWithRating)
+                   
+                }
+                    }
+            else {
+                // Log details of the failure
+                self.displayAlertWithTitle("Oops", message: "\(error!) \(error!.userInfo)")
+            }
+               
+                for roomNumber in 0 ..< self.dataArrayWithRoomNumber.count{
+                    self.dataArrayWithRoomStrings.addObject(String(self.dataArrayWithRoomNumber[(roomNumber)]))
+                    
+                }
+                for roomNumber in 0 ..< self.dataArrayWithRoomNumber.count {
+//                    self.dataDictionary[self.dataArrayWithRoomStrings[roomNumber] as! String] = [self.dataArrayWithRating.addObject(roomNumber)] as! [[Int]]
+                    //print(self.dataArrayWithRoomNumber[roomNumber])
+                }
+               // print(self.dataDictionary)
+//
+        }
+    }
+    
+    // Pass in a String and it will be displayed in an alert view
+    func displayMessage(message:String) {
+        let alert = UIAlertController(title: "", message: message,
+                                      preferredStyle: .Alert)
+        let defaultAction = UIAlertAction(title:"OK", style: .Default, handler: nil)
+        alert.addAction(defaultAction)
+        self.presentViewController(alert,animated:true, completion:nil)
+    }
+    
+    // Pass in a String and it will be displayed in an alert view
+    func displayAlertWithTitle(title:String, message:String){
+        let alert:UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let defaultAction:UIAlertAction =  UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(defaultAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
 
+    
 }

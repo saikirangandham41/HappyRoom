751a332cb7eaaa7130efaf4fbb52e5341eb6ae21
Gandham,Sai Kiran <s525905@ch140009m.nwmissouri.edu>
2016-11-14 18:18:00 -0600

    PieChartView And storyboard in view

---
+++ HappyRoomiOSProject/PieChartView.swift
@@ -0,0 +1,21 @@
+//
+//  PieChartView.swift
+//  HappyRoomiOSProject
+//
+//  Created by Gandham,Sai Kiran on 11/14/16.
+//  Copyright © 2016 NWMSU. All rights reserved.
+//
+
+import UIKit
+
+class PieChartView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
